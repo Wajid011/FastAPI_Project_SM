4cfc5df6586f01b53f8e8f61e1fb986baec3230c
Wajid011 <wajidibrahim4061@gmail.com>
2022-08-17 16:52:34 +0500

    initial commit

---
+++ alembic/versions/86646af30081_create_posts_table.py
@@ -0,0 +1,24 @@
+"""(create posts table)
+
+Revision ID: 86646af30081
+Revises: 132ab6f248c0
+Create Date: 2022-08-16 23:02:54.315747
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '86646af30081'
+down_revision = '132ab6f248c0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
